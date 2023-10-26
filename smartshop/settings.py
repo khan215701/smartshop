89ae7757c3541cbd9ee88518ae9c8651c1690efc
khan safiq <>
2023-10-26 13:08:46 +0530

    change media from images and configure media

--- smartshop/settings.py
+++ smartshop/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS += [
     'backend',
     'backend.user',
     'backend.category',
+    'backend.store',
 ]
 
 # Third-party Apps
@@ -141,6 +142,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
